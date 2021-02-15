1ffa7b6b163889081a4a5c5ef009e560288b8bb6
szocei <szocei.zoltan@gmail.com>
2021-02-15 18:39:43 +0100

    nav, home,heroes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './common/nav/nav.component';
+import { HomeComponent } from './common/home/home.component';
+import { HeroesComponent } from './common/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
